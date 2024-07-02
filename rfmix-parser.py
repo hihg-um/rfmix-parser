21d02ce0463375403079e8e91024fb32c1f8897c
Kyle Scott <kms309@miami.edu>
2024-07-02 11:15:53 -0400

    update parser to simplify usage and include gts
    Signed-off-by: Kyle Scott <kms309@miami.edu>

--- rfmix-parser.py
+++ rfmix-parser.py
@@ -2,129 +2,157 @@
 # SPDX-License-Identifier: GPL-3.0
 # Kyle M. Scott - kms309@miami.edu
 
-import argparse
 import pandas as pd
+import argparse
+from pysam import bcftools
+import io
+import os
+
 
 def parser():
-	parser = argparse.ArgumentParser(description='Parse RFMix output')
-	parser.add_argument('-f', '--mspFiles', required=True, help='File listing RFMix MSP output files, one per line')
-	parser.add_argument('-r', '--regions', required=True, help='File listing regions to parse, tab-separated with columns CHR, SPOS, EPOS')
-	parser.add_argument('-s', '--samples', required=False, help='File listing samples to include, one per line')
-	parser.add_argument('-o', '--out', required=True, help='Output file')
-	args = parser.parse_args()
-	return args
+    parser = argparse.ArgumentParser(description='Parse RFMix output')
+    parser.add_argument('-f', '--mspFile', required=True, help='Path to RFMix .msp.tsv output file')
+    parser.add_argument('-r', '--regions', required=True,
+                        help='List with region to parse, comma-separated with CHR,SPOS,EPOS')
+    parser.add_argument('-m', '--markers', required=False,
+                        help='List with markers to append to output, providing genotype and ancestry, comma-separated in format chr:pos')
+    parser.add_argument('-v', '--vcf', required=False,
+                        help='Path to Phased VCF file, must be indexed. Must be used in conjunction with --markers')
+    parser.add_argument('-o', '--out', required=True, help='Output file')
+    args = parser.parse_args()
+    return args
 
-def check_first_line(files: list):
-	first_line = None
-	for file in files:
-		with open(file, 'r') as f:
-			if first_line is None:
-				first_line = f.readline().strip()
-			elif first_line != f.readline().strip():
-				raise ValueError(f"Subpopulation codes do not match in all files. Ensure RFMix results come from the same run.")
-	return
 
-def get_population_mapping(files: list) -> pd.DataFrame:
-	file_path = files[0]
-	with open(file_path, 'r') as file:
-		populations = file.readline().replace('#Subpopulation order/codes: ', '').split('\t')
-	populations = [pop.strip().split('=') for pop in populations]
-	pop_df = pd.DataFrame(populations, columns=['POP', 'NUM']).astype(str)
-	return pop_df
+def get_population_mapping(msp_file: str) -> pd.DataFrame:
+    with open(msp_file, 'r') as file:
+        populations = file.readline().replace('#Subpopulation order/codes: ', '').split('\t')
+    populations = [pop.strip().split('=') for pop in populations]
+    pop_df = pd.DataFrame(populations, columns=['POP', 'NUM']).astype(str)
+    return pop_df
 
-def read_msp(files: list) -> pd.DataFrame:
-	dfs = []
-	for file in files:
-		df = pd.read_csv(file, sep='\t', skiprows=1, dtype=str)
-		column_types = {'spos': int, 'epos': int, 'n snps': int, 'sgpos': float, 'egpos': float}
-		for col, col_type in column_types.items():
-			df[col] = df[col].astype(col_type)
-		dfs.append(df)
-	columns = [df.columns.tolist() for df in dfs]
-	if not all([col == columns[0] for col in columns]):
-		raise ValueError(f"Columns do not match in all files. Ensure RFMix results come from the same run.")
-	return pd.concat(dfs)
 
-def subset_samples(sample_list: list, msp: pd.DataFrame) -> pd.DataFrame:
-	columns = []
-	for sample in sample_list:
-		columns.extend([f'{sample}.0', f'{sample}.1'])
-	columns = ['#chm', 'spos', 'epos', 'n snps', 'sgpos', 'egpos'] + columns
-	return msp[columns]
+def read_msp(msp_file: str) -> pd.DataFrame:
+    df = pd.read_csv(msp_file, sep='\t', skiprows=1, dtype=str)
+    column_types = {'spos': int, 'epos': int, 'n snps': int, 'sgpos': float, 'egpos': float}
+    for col, col_type in column_types.items():
+        df[col] = df[col].astype(col_type)
+    return df
 
-def parse_regions(regions: pd.DataFrame, rfmix: pd.DataFrame, pop_map: pd.DataFrame) -> pd.DataFrame:
-	out_df = pd.DataFrame(columns=['ID', 'CHR', 'SPOS', 'EPOS', 'Ancestry', 'Proportion'])
-	i = 0
-	while i < len(regions):
-		region_row = regions.iloc[i]
-		if region_row['CHR'] not in rfmix['#chm'].values:
-			i += 1
-			continue
-		working_rfmix = rfmix.copy()
-		mask_chr = working_rfmix['#chm'] == region_row['CHR']
-		mask1 = mask_chr & (working_rfmix['spos'] < region_row['SPOS']) & (working_rfmix['epos'] > region_row['SPOS'])
-		mask2 = mask_chr & (working_rfmix['spos'] > region_row['SPOS']) & (working_rfmix['epos'] < region_row['EPOS'])
-		mask3 = mask_chr & (working_rfmix['spos'] < region_row['EPOS']) & (working_rfmix['epos'] > region_row['EPOS'])
-		mask4 = mask_chr & (working_rfmix['spos'] < region_row['SPOS']) & (working_rfmix['epos'] > region_row['EPOS'])
-		mask = mask1 | mask2 | mask3 | mask4
-		working_rfmix.loc[mask1, 'spos'] = region_row['SPOS']
-		working_rfmix.loc[mask3, 'epos'] = region_row['EPOS']
-		working_rfmix.loc[mask4, 'spos'] = region_row['SPOS']
-		working_rfmix.loc[mask4, 'epos'] = region_row['EPOS']
-		rfmix_region = working_rfmix[mask]
 
-		pop_dict = pop_map.set_index('NUM')['POP'].to_dict()
-		rfmix_region.iloc[:, 6:] = rfmix_region.iloc[:, 6:].replace(pop_dict)
+def parse_region(chm: str, spos: int, epos: int, msp: pd.DataFrame, pop_map: pd.DataFrame) -> pd.DataFrame:
+    out_df = pd.DataFrame(columns=['ID', 'CHR', 'SPOS', 'EPOS', 'REGION_ANCESTRY', 'REGION_PROPORTION'])
+    if chm not in msp['#chm'].values:
+        raise ValueError(f"Chromosome {chm} not found in RFMix output. This could be a 'chr' prefix issue.")
+    mask_chr = msp['#chm'] == chm
+    mask1 = mask_chr & (msp['spos'] < spos) & (msp['epos'] > epos)
+    mask2 = mask_chr & (msp['spos'] > spos) & (msp['epos'] < epos)
+    mask3 = mask_chr & (msp['spos'] < spos) & (msp['epos'] > epos)
+    mask4 = mask_chr & (msp['spos'] < spos) & (msp['epos'] > epos)
+    mask = mask1 | mask2 | mask3 | mask4
+    msp.loc[mask1, 'spos'] = spos
+    msp.loc[mask3, 'epos'] = epos
+    msp.loc[mask4, 'spos'] = spos
+    msp.loc[mask4, 'epos'] = epos
+    msp_region = msp[mask]
 
-		segment_lengths = {col: [] for col in rfmix_region.columns[6:]}
-		total_length = rfmix_region['epos'].max() - rfmix_region['spos'].min()
+    pop_dict = pop_map.set_index('NUM')['POP'].to_dict()
+    msp_region.iloc[:, 6:] = msp_region.iloc[:, 6:].replace(pop_dict)
 
-		for col in rfmix_region.columns[6:]:
-			start = rfmix_region.iloc[0]['spos']
-			prev_value = rfmix_region.iloc[0][col]
-			for j in range(1, len(rfmix_region)):
-				if rfmix_region.iloc[j][col] != prev_value:
-					end = rfmix_region.iloc[j-1]['epos']
-					segment_lengths[col].append([prev_value, ((end - start)/total_length).round(2)])
-					start = rfmix_region.iloc[j]['spos']
-					prev_value = rfmix_region.iloc[j][col]
-			end = rfmix_region.iloc[len(rfmix_region)-1]['epos']
-			segment_lengths[col].append([prev_value, ((end - start)/total_length).round(2)])
+    segment_lengths = {col: [] for col in msp_region.columns[6:]}
+    total_length = msp_region['epos'].max() - msp_region['spos'].min()
 
-		processed_data = []
-		for key, values in segment_lengths.items():
-			id_ = key.rsplit('.', 1)[0]
-			ancestry = ':'.join([str(v[0]) for v in values])
-			proportion = ':'.join([str(v[1]) for v in values])
-			processed_data.append([id_, ancestry, proportion])
+    for col in msp_region.columns[6:]:
+        start = msp_region.iloc[0]['spos']
+        prev_value = msp_region.iloc[0][col]
+        for j in range(1, len(msp_region)):
+            if msp_region.iloc[j][col] != prev_value:
+                end = msp_region.iloc[j - 1]['epos']
+                segment_lengths[col].append([prev_value, ((end - start) / total_length).round(2)])
+                start = msp_region.iloc[j]['spos']
+                prev_value = msp_region.iloc[j][col]
+        end = msp_region.iloc[len(msp_region) - 1]['epos']
+        segment_lengths[col].append([prev_value, ((end - start) / total_length).round(2)])
 
-		processed_df = pd.DataFrame(processed_data, columns=['ID', 'Ancestry', 'Proportion'])
-		processed_df['CHR'] = region_row['CHR']
-		processed_df['SPOS'] = rfmix_region['spos'].min()
-		processed_df['EPOS'] = rfmix_region['epos'].max()
-		processed_df = processed_df.groupby('ID').agg({
-			'CHR': 'first',
-			'SPOS': 'first',
-			'EPOS': 'first',
-			'Ancestry': '|'.join,
-			'Proportion': '|'.join
-		}).reset_index()
-		out_df = pd.concat([out_df, processed_df])
-		i += 1
-	return out_df
+    processed_data = []
+    for key, values in segment_lengths.items():
+        id_ = key.rsplit('.', 1)[0]
+        ancestry = ':'.join([str(v[0]) for v in values])
+        proportion = ':'.join([str(v[1]) for v in values])
+        processed_data.append([id_, ancestry, proportion])
+
+    processed_df = pd.DataFrame(processed_data, columns=['ID', 'REGION_ANCESTRY', 'REGION_PROPORTION'])
+    processed_df['CHR'] = chm
+    processed_df['SPOS'] = msp_region['spos'].min()
+    processed_df['EPOS'] = msp_region['epos'].max()
+    processed_df = processed_df.groupby('ID').agg({
+        'CHR': 'first',
+        'SPOS': 'first',
+        'EPOS': 'first',
+        'REGION_ANCESTRY': '|'.join,
+        'REGION_PROPORTION': '|'.join
+    }).reset_index()
+    out_df = pd.concat([out_df, processed_df])
+    return out_df
+
+
+def get_genotype(vcf_file: str, markers: list) -> pd.DataFrame:
+    header = bcftools.view(vcf_file, '-h')
+    catch_str = "#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	"
+    header = 'MARKERS\t' + header.split(catch_str)[1]
+    query = bcftools.query(vcf_file, '-r', f'{markers}', '-f', '%CHROM:%POS[\t%GT]\n')
+    vcf_df = pd.read_csv(io.StringIO(header + query), sep='\t')
+    if len(vcf_df) > 1:
+        vcf_df["TEMP"] = 0
+        vcf_df = vcf_df.groupby('TEMP').agg(lambda x: ','.join(x)).reset_index()
+        vcf_df = vcf_df.drop('TEMP', axis=1)
+    return vcf_df
+
+
+def get_marker_ancestry(markers: list, msp: pd.DataFrame, pop_map: pd.DataFrame) -> pd.DataFrame:
+    marker_ancestry_df = pd.DataFrame()
+    for marker in markers.split(','):
+        chm, pos = marker.split(':')
+        mask_chr = msp['#chm'] == chm
+        mask_pos = (msp['spos'] <= int(pos)) & (msp['epos'] >= int(pos))
+        mask = mask_chr & mask_pos
+        ancestry = msp[mask].iloc[:, 6:].replace(pop_map.set_index('NUM')['POP'].to_dict())
+        ancestry.insert(0, 'MARKERS', marker)
+        ids = ancestry.columns[1:].str.split('.').str[0].unique().tolist()
+        res = pd.wide_to_long(ancestry, ids, i="MARKERS", j="suffix_key", suffix='.*').reset_index().drop('suffix_key', axis=1)
+        res = res.groupby('MARKERS').agg(lambda x: '|'.join(x)).reset_index()
+        marker_ancestry_df = pd.concat([marker_ancestry_df, res])
+    if len(marker_ancestry_df) > 1:
+        marker_ancestry_df["TEMP"] = 0
+        marker_ancestry_df = marker_ancestry_df.groupby('TEMP').agg(lambda x: ','.join(x)).reset_index()
+        marker_ancestry_df = marker_ancestry_df.drop('TEMP', axis=1)
+    return marker_ancestry_df
+
+
+def transpose_marker_results(vcf_df: pd.DataFrame, marker_df: pd.DataFrame) -> pd.DataFrame:
+    vcf_df = vcf_df.set_index('MARKERS').reset_index()
+    vcf_df = vcf_df.melt(id_vars='MARKERS', var_name='ID', value_name='MARKER_GT')
+    marker_df = marker_df.set_index('MARKERS').reset_index()
+    marker_df = marker_df.melt(id_vars='MARKERS', var_name='ID', value_name='MARKER_ANCESTRY')
+    return pd.merge(vcf_df, marker_df, on=['MARKERS', 'ID'])
 
 def main():
-	args = parser()
-	files = [line.strip() for line in open(args.mspFiles, 'r')]
-	regions_df = pd.read_csv(args.regions, sep='\t')
-	check_first_line(files)
-	pop_map = get_population_mapping(files)
-	msp = read_msp(files)
-	if args.samples:
-		sample_list = [line.strip() for line in open(args.samples, 'r')]
-		msp = subset_samples(sample_list, msp)
-	out_df = parse_regions(regions_df, msp, pop_map)
-	out_df.to_csv(args.out, sep='\t', index=False)
+    args = parser()
+    pop_map = get_population_mapping(args.mspFile)
+    msp = read_msp(args.mspFile)
+    region = args.regions.split(',')
+    if len(region) != 3:
+        raise ValueError('Region must contain exactly three entries and be in format CHR,SPOS,EPOS')
+    chm = region[0]
+    spos = int(region[1])
+    epos = int(region[2])
+    anc_prop_df = parse_region(chm, spos, epos, msp, pop_map)
+    if args.markers and args.vcf:
+        vcf_df = get_genotype(args.vcf, args.markers)
+        marker_df = get_marker_ancestry(args.markers, msp, pop_map)
+        final_marker_df = transpose_marker_results(vcf_df, marker_df)
+        anc_prop_df = pd.merge(anc_prop_df, final_marker_df, on='ID')
+    anc_prop_df.to_csv(args.out, sep='\t', index=False)
+
 
 if __name__ == '__main__':
-	main()
+    main()
